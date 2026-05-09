90cae0ef1dcb19dd7abe730cc930c5d6faca52cf
Akhil <Akhil@email.com>
2026-05-09 11:34:44 +0530

    validation

--- app/transformation.py
+++ app/transformation.py
@@ -1,10 +1,26 @@
-from pyspark.sql.functions import col, sum
+from pyspark.sql.functions import col, sum, when
 
 def transform_data(df):
+
+    # Remove duplicates
     df = df.dropDuplicates()
 
+    # Cast column
     df = df.withColumn("sales_amount", col("sales_amount").cast("int"))
 
+    # 🔥 Handle NULL values
+    df = df.withColumn(
+        "sales_amount",
+        when(col("sales_amount").isNull(), 0).otherwise(col("sales_amount"))
+    )
+
+    # 🔥 Handle negative values
+    df = df.withColumn(
+        "sales_amount",
+        when(col("sales_amount") < 0, 0).otherwise(col("sales_amount"))
+    )
+
+    # Aggregation
     result = df.groupBy("product_id") \
         .agg(sum("sales_amount").alias("total_sales"))
 
